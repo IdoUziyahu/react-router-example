71046141df012e5ec079b44f677d109fdeecb1c3
IdoUziyahu <idouziyahu2002@gmail.com>
2024-05-16 13:37:09 +0300

    react router example

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ComponentA from "./ComponentA";
+import ComponentB from "./ComponentB";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Example App</h1>
+      <h3>try to go to paths '/a' and '/b'</h3>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/a" element={<ComponentA />} />
+          <Route path="/b" element={<ComponentB />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
